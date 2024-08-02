45c8e653e04611aaf3e19c8c405a433ba06d6950
Cristian Manuel De León Monzón <cristiandeleonmonzon@gmail.com>
2024-08-02 19:22:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ async def get_characters(skip: int = None, limit: int = None):
             results = session.exec(statement).all()
 
         res = [i for i in results]
-        return {"results": res}
+        return res
 
 @app.get("/characters/{character_id}", response_model=TheBoys)
 async def get_character_by_id(character_id: int):
